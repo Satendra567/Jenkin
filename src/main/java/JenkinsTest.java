0b0dd77eae3fe0cdf43d929d09bb2745c95e654a
Satendra Singh <satendrasingh567@gmail.com>
2026-02-16 23:46:09 +0530

    new changes

--- src/main/java/JenkinsTest.java
+++ src/main/java/JenkinsTest.java
@@ -2,5 +2,6 @@
 public class JenkinsTest {
     public static void main(String arg []){
         System.out.println("this is first configuration with jenkins");
+        System.out.println("This is for jenkins new learning");
     }
 }
